4b8288154c9072aab47a064149eb1130d5955d11
Takedzaa <optman33@mail.ru>
2024-06-28 12:46:09 +0700

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace CSharpProject
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
